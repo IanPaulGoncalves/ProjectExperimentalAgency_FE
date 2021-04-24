a55516e76677b3ed3e162dcca872f0182e3cc296
IanPaulGoncalves <63078125+IanPaulGoncalves@users.noreply.github.com>
2021-04-23 23:05:46 -0300

    test navegation

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import NavegationBar from '../components/Navegation/NavegationBar';
 import '../styles/style.css';
 
 function App() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center' }}>
-      <h1>Hello, guys!!!</h1>
-    </div>
+    <NavegationBar />
   );
 }
 
